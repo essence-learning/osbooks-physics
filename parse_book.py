fc8aee5f465deb2e45c6b42ee16ed67f46383977
Steven He <hess1@uci.edu>
2024-06-29 18:47:41 -0700

    More robust parsing, filtering teacher notes

--- parse_book.py
+++ parse_book.py
@@ -1,6 +1,8 @@
 import xml.etree.ElementTree as ET
 import json
-import re
+import os
+
+from pathlib import Path
 
 def parse_xml_to_json(xml_file):
     tree = ET.parse(xml_file)
@@ -11,7 +13,10 @@ def parse_xml_to_json(xml_file):
         for element in collection:
             tag = element.tag.split('}')[-1]
             if tag == "module":
-                parsed_collection["pages"].append(element.attrib["document"])
+                parsed_collection["pages"].append({
+                    "id": element.attrib["document"],
+                    "title" : "page title"
+                })
             elif tag == "subcollection":
                 title = element.find('{http://cnx.rice.edu/mdml}title').text
                 subsection = {
@@ -26,14 +31,18 @@ def parse_xml_to_json(xml_file):
     return parsed_data
 
 
-def cnxml_to_mdx(cnxml_content):
-    root = ET.fromstring(cnxml_content)
+def cnxml_to_mdx(cnxml_file):
+    root = ET.parse(cnxml_file)
     output_mdx = ""
+    content_id = ""
+    content_title = ""
 
     def parse_element(element):
+        print(f'recursive parse of {element}')
         tag = element.tag.split('}')[-1]
         if tag == 'title':
-            return f'# {element.text}\n\n'
+            content_title = element.text
+            return f'# {element.text}\n'
         elif tag == 'para':
             return f'{parse_para(element)}\n\n'
         elif tag == 'section':
@@ -46,10 +55,16 @@ def cnxml_to_mdx(cnxml_content):
             return f'[{element.text}]({element.attrib.get("url")})'
         elif tag == 'sup':
             return f'<sup>{element.text}</sup>'
-        elif element.text:
-            return element.text
-        else:
-            return ''.join(parse_element(e) for e in element)
+        elif tag == 'metadata':
+            for e in element:
+                if 'content-id' in e.tag:
+                    content_id = e.text
+                    break
+        elif tag == 'note':
+            if element.attrib['class'] == 'os-teacher':
+                return ''
+
+        return ''.join(parse_element(e) for e in element)
 
     def parse_para(element):
         content = ""
@@ -83,31 +98,41 @@ def cnxml_to_mdx(cnxml_content):
                 output += f'{parse_element(item)}\n'
         return output
 
-    for element in root.findall('.//'):
+    for element in root.findall('./*'):
+        print(f'top level parse of {element}')
         output_mdx += parse_element(element)
 
-    return output_mdx
+    return content_id, content_title, output_mdx
 
 def write_mdx():
-    with open('./modules/m54082/index.cnxml', 'r', encoding='utf-8') as f:
-        cnxml_content = f.read()
+    cnxml_file = Path('./modules/m54083/index.cnxml')
+    # with open('./modules/m54083/index.cnxml', 'r', encoding='utf-8') as f:
+    #     cnxml_content = f.read()
 
-    mdx_content = cnxml_to_mdx(cnxml_content)
+    id, title, mdx_content = cnxml_to_mdx(cnxml_file)
+    print(f'id: {id}, title: {title}')
 
     with open('output.mdx', 'w', encoding='utf-8') as f:
         f.write(mdx_content)
 
+    return id, title
+
 
 if __name__ == "__main__":
-    title = 'Physics'
-    xml_file = './collections/physics.collection.xml'
-    json_file = f'{title}.json'
+    book_title = 'Physics'
+    directory_path = Path.cwd() / Path(book_title)
+
+    if not directory_path.exists():
+        os.makedirs(directory_path)
+
+    xml_file = Path.cwd() / Path('collections/physics.collection.xml')
+    toc_file = directory_path / Path('toc.json')
 
     write_mdx()
 
     # Convert TOC stuff
-    parsed_data = {'title': title}
+    parsed_data = {'title': book_title}
     parsed_data.update(parse_xml_to_json(xml_file))
 
-    with open(json_file, 'w') as f:
+    with open(toc_file, 'w') as f:
         json.dump(parsed_data, f, indent=4)
