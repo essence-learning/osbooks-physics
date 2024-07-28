f3a0d829698b39762680a5996d46cbf561dfe08c
Steven He <hess1@uci.edu>
2024-07-27 21:42:27 -0700

    Fixing TOC retroactive renaming

--- parser.py
+++ parser.py
@@ -1,7 +1,6 @@
 from pathlib import Path
+import json
 import xml.etree.ElementTree as ET
-
-COLLECTION_FILE = 'physics.collections.xml'
 
 class Module:
     def __init__(self):
@@ -13,9 +12,12 @@ class Module:
     def __repr__(self):
         return f'Title: {self.title}, id: {self.id}, content_length: {len(self.content)}, glossary_length: {len(self.glossary)}'
 
-def tag(element):
-    '''Returns tag of element with namespace removed'''
-    return element.tag.split('}', 1)[1]
+def remove_namespace(element):
+    '''Removes all namespaces from XML tree'''
+    for elem in element.iter():
+        if isinstance(elem.tag, str):
+            elem.tag = elem.tag.split('}')[-1]
+    return element
 
 def text(element, section_depth):
     '''Returns parsed inner XML'''
@@ -28,9 +30,7 @@ def text(element, section_depth):
         if child.tail:
             content += child.tail
 
-    content.strip()
-
-    return content
+    return content.strip()
 
 def generate_content(node, section_depth) -> str:
     if 'class' in node.attrib and node.attrib['class'] == 'os-teacher':
@@ -38,26 +38,32 @@ def generate_content(node, section_depth) -> str:
 
     output = []
 
-    match tag(node):
-        case 'content' | 'section':
+    match node.tag:
+        case 'content' | 'section' | 'list':
             for child in node:
                 output.append(generate_content(child, section_depth + 1))
 
+        case 'newline':
+            return '\n'
+
         case 'para':
-            output.append(text(node, section_depth))
+            output.append(text(node, section_depth) + '\n')
 
         case 'title':
             output.append(f'{"#" * section_depth} {text(node, section_depth)}')
 
+        case 'item':
+            output.append(f'* {text(node, section_depth)}')
+
         case 'emphasis':
-            effect = ''
+            effect = '**'
 
             if 'effect' in node.attrib:
                 match node.attrib['effect']:
                     case 'italics':
                         effect = '*'
                     case _:
-                        effect = ''
+                        effect = '**'
 
             output.append(f'{effect}{text(node, section_depth)}{effect}')
 
@@ -76,13 +82,13 @@ def process_module(root) -> Module:
     current_module = Module()
 
     for component in root:
-        match tag(component):
+        match component.tag:
             case 'title':
                 current_module.title = component.text
 
             case 'metadata':
                 for metadata in component:
-                    if tag(metadata) == 'content-id':
+                    if metadata.tag == 'content-id':
                         current_module.id = metadata.text
 
             case 'content':
@@ -94,28 +100,70 @@ def process_module(root) -> Module:
             case _:
                 pass
 
+    current_module.content = f'# {current_module.title}\n' + current_module.content
+
     return current_module
+
+def process_toc(xml_file, module_table):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    remove_namespace(root)
+
+    def parse_collection(node):
+        if node.tag == 'module':
+            module_id = node.attrib['document']
+            module_title = module_table[module_id].title if module_id in module_table else 'No Title Found'
+            parsed_collection = {'is_page': True, 'title': module_title, 'id': module_id}
+        else:
+            parsed_collection = {'is_page': False, 'title': 'No Title Found', 'subsections': []}
+
+            for element in node:
+                if element.tag == 'title':
+                    parsed_collection['title'] = element.text
+                else:
+                    parsed_collection['subsections'].append(parse_collection(element))
+
+        return parsed_collection
+
+    parsed_data = parse_collection(root.find('content'))
+
+    # I don't know why pyright is bugging out over this line but it works fine
+    parsed_data['title'] = root.find('metadata').find('title').text
+
+    return parsed_data
 
 
 if __name__ == '__main__':
+    BOOK_ID = 'physics'
+
     cwd = Path.cwd()
-    
+    collection_file = cwd / Path(f'collections/{BOOK_ID}.collection.xml')
+
     module_table: dict[str, Module] = dict()
 
-    # First parse the modules
+    # Parse the modules
     # modules = [m for m in (cwd / 'modules').iterdir() if m.is_dir()]
-    modules = [cwd / 'modules/m54081']
+    modules = [cwd / 'modules/m54082']
     for module in modules:
         print(f'Processing {module.name}')
         module_tree = ET.parse(module / 'index.cnxml')
         module_root = module_tree.getroot()
 
+        remove_namespace(module_root)
+
         module_obj = process_module(module_root)
         print(module_obj)
+
         # For each module, write conent to mdx file
+        with open(cwd / Path(f'content/{module_obj.id}.mdx'), 'w', encoding='utf-8') as f:
+            f.write(module_obj.content)
 
         # Then, add to module table for later access to metadata
         module_table[module_obj.id] = module_obj
         print('=' * 20)
         print(module_obj.content)
 
+    # Generate the TOC
+    toc_data = process_toc(collection_file, module_table)
+    print(json.dumps(toc_data, indent=2))
